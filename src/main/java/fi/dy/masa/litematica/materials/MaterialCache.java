maruohon/litematica

Schematic Rebuild: Allow using a set/stored block state with an empty hand

src/main/java/fi/dy/masa/litematica/materials/MaterialCache.java
@@ -43,7 +43,8 @@ public class MaterialCache
 {
     private static final MaterialCache INSTANCE = new MaterialCache();
 
-    protected final IdentityHashMap<IBlockState, ItemStack> itemsForStates = new IdentityHashMap<>();
+    protected final IdentityHashMap<IBlockState, ItemStack> buildItemsForStates = new IdentityHashMap<>();
+    protected final IdentityHashMap<IBlockState, ItemStack> displayItemsForStates = new IdentityHashMap<>();
     protected final WorldSchematic tempWorld;
     protected final BlockPos checkPos;
     protected boolean hasReadFromFile;
@@ -71,29 +72,41 @@ public class MaterialCache
 
     public void clearCache()
     {
-        this.itemsForStates.clear();
+        this.buildItemsForStates.clear();
     }
 
-    public ItemStack getItemForState(IBlockState state)
+    public ItemStack getRequiredBuildItemForState(IBlockState state)
     {
-        return this.getItemForState(state, this.tempWorld, this.checkPos);
+        return this.getRequiredBuildItemForState(state, this.tempWorld, this.checkPos);
     }
 
-    public ItemStack getItemForState(IBlockState state, World world, BlockPos pos)
+    public ItemStack getRequiredBuildItemForState(IBlockState state, World world, BlockPos pos)
     {
-        ItemStack stack = this.itemsForStates.get(state);
+        ItemStack stack = this.buildItemsForStates.get(state);
 
         if (stack == null)
         {
-            stack = this.getItemForStateFromWorld(state, world, pos);
+            stack = this.getItemForStateFromWorld(state, world, pos, true);
         }
 
         return stack;
     }
 
-    protected ItemStack getItemForStateFromWorld(IBlockState state, World world, BlockPos pos)
+    public ItemStack getItemForDisplayNameForState(IBlockState state)
     {
-        ItemStack stack = this.getStateToItemOverride(state);
+        ItemStack stack = this.displayItemsForStates.get(state);
+
+        if (stack == null)
+        {
+            stack = this.getItemForStateFromWorld(state, this.tempWorld, this.checkPos, false);
+        }
+
+        return stack;
+    }
+
+    protected ItemStack getItemForStateFromWorld(IBlockState state, World world, BlockPos pos, boolean isBuildItem)
+    {
+        ItemStack stack = isBuildItem ? this.getStateToItemOverride(state) : null;
 
         if (stack == null)
         {
@@ -110,7 +123,15 @@ public class MaterialCache
             this.overrideStackSize(state, stack);
         }
 
-        this.itemsForStates.put(state, stack);
+        if (isBuildItem)
+        {
+            this.buildItemsForStates.put(state, stack);
+        }
+        else
+        {
+            this.displayItemsForStates.put(state, stack);
+        }
+
         this.dirty = true;
 
         return stack;
@@ -175,7 +196,7 @@ public class MaterialCache
             }
         }
 
-        return ImmutableList.of(this.getItemForState(state, world, pos));
+        return ImmutableList.of(this.getRequiredBuildItemForState(state, world, pos));
     }
 
     @Nullable
@@ -256,9 +277,18 @@ public class MaterialCache
     protected NBTTagCompound writeToNBT()
     {
         NBTTagCompound nbt = new NBTTagCompound();
+
+        nbt.setTag("MaterialCache", this.writeMapToNBT(this.buildItemsForStates));
+        nbt.setTag("DisplayMaterialCache", this.writeMapToNBT(this.displayItemsForStates));
+
+        return nbt;
+    }
+
+    protected NBTTagList writeMapToNBT(IdentityHashMap<IBlockState, ItemStack> map)
+    {
         NBTTagList list = new NBTTagList();
 
-        for (Map.Entry<IBlockState, ItemStack> entry : this.itemsForStates.entrySet())
+        for (Map.Entry<IBlockState, ItemStack> entry : map.entrySet())
         {
             NBTTagCompound tag = new NBTTagCompound();
             NBTTagCompound stateTag = new NBTTagCompound();
@@ -270,18 +300,23 @@ public class MaterialCache
             list.appendTag(tag);
         }
 
-        nbt.setTag("MaterialCache", list);
-
-        return nbt;
+        return list;
     }
 
-    protected boolean readFromNBT(NBTTagCompound nbt)
+    protected void readFromNBT(NBTTagCompound nbt)
     {
-        this.itemsForStates.clear();
+        this.buildItemsForStates.clear();
+        this.displayItemsForStates.clear();
 
-        if (nbt.hasKey("MaterialCache", Constants.NBT.TAG_LIST))
+        this.readMapFromNBT(nbt, "MaterialCache", this.buildItemsForStates);
+        this.readMapFromNBT(nbt, "DisplayMaterialCache", this.displayItemsForStates);
+    }
+
+    protected void readMapFromNBT(NBTTagCompound nbt, String tagName, IdentityHashMap<IBlockState, ItemStack> map)
+    {
+        if (nbt.hasKey(tagName, Constants.NBT.TAG_LIST))
         {
-            NBTTagList list = nbt.getTagList("MaterialCache", Constants.NBT.TAG_COMPOUND);
+            NBTTagList list = nbt.getTagList(tagName, Constants.NBT.TAG_COMPOUND);
             final int count = list.tagCount();
 
             for (int i = 0; i < count; ++i)
@@ -296,15 +331,11 @@ public class MaterialCache
                     if (state != null)
                     {
                         ItemStack stack = new ItemStack(tag.getCompoundTag("Item"));
-                        this.itemsForStates.put(state, stack);
+                        this.buildItemsForStates.put(state, stack);
                     }
                 }
             }
-
-            return true;
         }
-
-        return false;
     }
 
     protected File getCacheDir()

src/main/java/fi/dy/masa/litematica/render/infohud/ToolHud.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import java.util.List;
 import fi.dy.masa.litematica.config.Configs;
 import fi.dy.masa.litematica.data.DataManager;
-import fi.dy.masa.litematica.materials.MaterialCache;
 import fi.dy.masa.litematica.schematic.placement.SchematicPlacement;
 import fi.dy.masa.litematica.schematic.placement.SubRegionPlacement;
 import fi.dy.masa.litematica.schematic.projects.SchematicProject;
@@ -253,6 +252,15 @@ public class ToolHud extends InfoHud
 
                 lines.add(StringUtils.translate("litematica.hud.area_selection.origin", green + str + rst));
 
+                IBlockState state = mode.getPrimaryBlock();
+                ItemStack stack = this.mc.player.getHeldItemMainhand();
+
+                if (state != null && mode == ToolMode.REBUILD &&
+                    (stack.isEmpty() || EntityUtils.areStacksEqualIgnoreDurability(stack, DataManager.getToolItem())))
+                {
+                    lines.add(StringUtils.translate("litematica.tool_hud.block_1", this.getBlockString(state)));
+                }
+
                 SubRegionPlacement placement = schematicPlacement.getSelectedSubRegionPlacement();
 
                 if (placement != null)
@@ -312,13 +320,12 @@ public class ToolHud extends InfoHud
 
     protected String getBlockString(IBlockState state)
     {
-        ItemStack stack = MaterialCache.getInstance().getItemForState(state);
         String strBlock;
 
         String green = GuiBase.TXT_GREEN;
         String rst = GuiBase.TXT_RST;
 
-        strBlock = green + stack.getDisplayName() + rst;
+        strBlock = green + state.getBlock().getLocalizedName() + rst;
         EnumFacing facing = BlockUtils.getFirstPropertyFacingValue(state);
 
         if (facing != null)

src/main/java/fi/dy/masa/litematica/util/WorldUtils.java
@@ -21,6 +21,7 @@ import fi.dy.masa.litematica.selection.Box;
 import fi.dy.masa.litematica.tool.ToolMode;
 import fi.dy.masa.litematica.util.PositionUtils.Corner;
 import fi.dy.masa.litematica.util.RayTraceUtils.RayTraceWrapper;
+import fi.dy.masa.litematica.util.RayTraceUtils.RayTraceWrapper.HitType;
 import fi.dy.masa.litematica.world.SchematicWorldHandler;
 import fi.dy.masa.litematica.world.WorldSchematic;
 import fi.dy.masa.malilib.gui.GuiBase;
@@ -331,13 +332,26 @@ public class WorldUtils
 
     public static void setToolModeBlockState(ToolMode mode, boolean primary, Minecraft mc)
     {
-        RayTraceResult trace = RayTraceUtils.getRayTraceFromEntity(mc.world, mc.player, true, 6);
         IBlockState state = Blocks.AIR.getDefaultState();
+        RayTraceWrapper wrapper = RayTraceUtils.getGenericTrace(mc.world, mc.player, 6, true);
 
-        if (trace != null &&
-            trace.typeOfHit == RayTraceResult.Type.BLOCK)
+        if (wrapper != null)
         {
-            state = mc.world.getBlockState(trace.getBlockPos());
+            RayTraceResult trace = wrapper.getRayTraceResult();
+
+            if (trace != null)
+            {
+                BlockPos pos = trace.getBlockPos();
+
+                if (wrapper.getHitType() == HitType.SCHEMATIC_BLOCK)
+                {
+                    state = SchematicWorldHandler.getSchematicWorld().getBlockState(pos);
+                }
+                else if (wrapper.getHitType() == HitType.VANILLA)
+                {
+                    state = mc.world.getBlockState(pos).getActualState(mc.world, pos);
+                }
+            }
         }
 
         if (primary)
@@ -373,7 +387,7 @@ public class WorldUtils
         {
             World world = SchematicWorldHandler.getSchematicWorld();
             IBlockState state = world.getBlockState(pos);
-            ItemStack stack = MaterialCache.getInstance().getItemForState(state, world, pos);
+            ItemStack stack = MaterialCache.getInstance().getRequiredBuildItemForState(state, world, pos);
 
             if (stack.isEmpty() == false)
             {
@@ -458,7 +472,7 @@ public class WorldUtils
             BlockPos pos = trace.getBlockPos();
             World world = SchematicWorldHandler.getSchematicWorld();
             IBlockState stateSchematic = world.getBlockState(pos);
-            ItemStack stack = MaterialCache.getInstance().getItemForState(stateSchematic);
+            ItemStack stack = MaterialCache.getInstance().getRequiredBuildItemForState(stateSchematic);
 
             // Already placed to that position, possible server sync delay
             if (easyPlaceIsPositionCached(pos))
@@ -729,7 +743,7 @@ public class WorldUtils
             }
 
             IBlockState stateSchematic = worldSchematic.getBlockState(pos);
-            ItemStack stack = MaterialCache.getInstance().getItemForState(stateSchematic);
+            ItemStack stack = MaterialCache.getInstance().getRequiredBuildItemForState(stateSchematic);
 
             // The player is holding the wrong item for the targeted position
             if (stack.isEmpty() == false && EntityUtils.getUsedHandForItem(mc.player, stack) == null)

EnderPhantomWing-Fork/litematica-printer

src/main/java/fi/dy/masa/litematica/materials/MaterialCache.java
@@ -41,7 +41,8 @@ public class MaterialCache
 {
     private static final MaterialCache INSTANCE = new MaterialCache();
 
-    protected final IdentityHashMap<BlockState, ItemStack> itemsForStates = new IdentityHashMap<>();
+    protected final IdentityHashMap<BlockState, ItemStack> buildItemsForStates = new IdentityHashMap<>();
+    protected final IdentityHashMap<BlockState, ItemStack> displayItemsForStates = new IdentityHashMap<>();
     protected final WorldSchematic tempWorld;
     protected final BlockPos checkPos;
     protected boolean hasReadFromFile;
@@ -67,29 +68,41 @@ public class MaterialCache
 
     public void clearCache()
     {
-        this.itemsForStates.clear();
+        this.buildItemsForStates.clear();
     }
 
-    public ItemStack getItemForState(BlockState state)
+    public ItemStack getRequiredBuildItemForState(BlockState state)
     {
-        return this.getItemForState(state, this.tempWorld, this.checkPos);
+        return this.getRequiredBuildItemForState(state, this.tempWorld, this.checkPos);
     }
 
-    public ItemStack getItemForState(BlockState state, World world, BlockPos pos)
+    public ItemStack getRequiredBuildItemForState(BlockState state, World world, BlockPos pos)
     {
-        ItemStack stack = this.itemsForStates.get(state);
+        ItemStack stack = this.buildItemsForStates.get(state);
 
         if (stack == null)
         {
-            stack = this.getItemForStateFromWorld(state, world, pos);
+            stack = this.getItemForStateFromWorld(state, world, pos, true);
         }
 
         return stack;
     }
 
-    protected ItemStack getItemForStateFromWorld(BlockState state, World world, BlockPos pos)
+    public ItemStack getItemForDisplayNameForState(BlockState state)
     {
-        ItemStack stack = this.getStateToItemOverride(state);
+        ItemStack stack = this.displayItemsForStates.get(state);
+
+        if (stack == null)
+        {
+            stack = this.getItemForStateFromWorld(state, this.tempWorld, this.checkPos, false);
+        }
+
+        return stack;
+    }
+
+    protected ItemStack getItemForStateFromWorld(BlockState state, World world, BlockPos pos, boolean isBuildItem)
+    {
+        ItemStack stack = isBuildItem ? this.getStateToItemOverride(state) : null;
 
         if (stack == null)
         {
@@ -106,7 +119,15 @@ public class MaterialCache
             this.overrideStackSize(state, stack);
         }
 
-        this.itemsForStates.put(state, stack);
+        if (isBuildItem)
+        {
+            this.buildItemsForStates.put(state, stack);
+        }
+        else
+        {
+            this.displayItemsForStates.put(state, stack);
+        }
+
         this.dirty = true;
 
         return stack;
@@ -138,7 +159,7 @@ public class MaterialCache
             return ImmutableList.of(new ItemStack(Blocks.FLOWER_POT), block.getPickStack(world, pos, state));
         }
 
-        return ImmutableList.of(this.getItemForState(state, world, pos));
+        return ImmutableList.of(this.getRequiredBuildItemForState(state, world, pos));
     }
 
     @Nullable
@@ -219,9 +240,18 @@ public class MaterialCache
     protected CompoundTag writeToNBT()
     {
         CompoundTag nbt = new CompoundTag();
+
+        nbt.put("MaterialCache", this.writeMapToNBT(this.buildItemsForStates));
+        nbt.put("DisplayMaterialCache", this.writeMapToNBT(this.displayItemsForStates));
+
+        return nbt;
+    }
+
+    protected ListTag writeMapToNBT(IdentityHashMap<BlockState, ItemStack> map)
+    {
         ListTag list = new ListTag();
 
-        for (Map.Entry<BlockState, ItemStack> entry : this.itemsForStates.entrySet())
+        for (Map.Entry<BlockState, ItemStack> entry : map.entrySet())
         {
             CompoundTag tag = new CompoundTag();
             CompoundTag stateTag = NbtHelper.fromBlockState(entry.getKey());
@@ -232,18 +262,23 @@ public class MaterialCache
             list.add(tag);
         }
 
-        nbt.put("MaterialCache", list);
-
-        return nbt;
+        return list;
     }
 
-    protected boolean readFromNBT(CompoundTag nbt)
+    protected void readFromNBT(CompoundTag nbt)
     {
-        this.itemsForStates.clear();
+        this.buildItemsForStates.clear();
+        this.displayItemsForStates.clear();
 
-        if (nbt.contains("MaterialCache", Constants.NBT.TAG_LIST))
+        this.readMapFromNBT(nbt, "MaterialCache", this.buildItemsForStates);
+        this.readMapFromNBT(nbt, "DisplayMaterialCache", this.displayItemsForStates);
+    }
+
+    protected void readMapFromNBT(CompoundTag nbt, String tagName, IdentityHashMap<BlockState, ItemStack> map)
+    {
+        if (nbt.contains(tagName, Constants.NBT.TAG_LIST))
         {
-            ListTag list = nbt.getList("MaterialCache", Constants.NBT.TAG_COMPOUND);
+            ListTag list = nbt.getList(tagName, Constants.NBT.TAG_COMPOUND);
             final int count = list.size();
 
             for (int i = 0; i < count; ++i)
@@ -258,15 +293,11 @@ public class MaterialCache
                     if (state != null)
                     {
                         ItemStack stack = ItemStack.fromTag(tag.getCompound("Item"));
-                        this.itemsForStates.put(state, stack);
+                        this.buildItemsForStates.put(state, stack);
                     }
                 }
             }
-
-            return true;
         }
-
-        return false;
     }
 
     protected File getCacheDir()

src/main/java/fi/dy/masa/litematica/render/infohud/ToolHud.java
@@ -3,9 +3,12 @@ package fi.dy.masa.litematica.render.infohud;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import fi.dy.masa.litematica.config.Configs;
 import fi.dy.masa.litematica.data.DataManager;
-import fi.dy.masa.litematica.materials.MaterialCache;
 import fi.dy.masa.litematica.schematic.placement.SchematicPlacement;
 import fi.dy.masa.litematica.schematic.placement.SubRegionPlacement;
 import fi.dy.masa.litematica.schematic.projects.SchematicProject;
@@ -23,10 +26,6 @@ import fi.dy.masa.malilib.config.HudAlignment;
 import fi.dy.masa.malilib.gui.GuiBase;
 import fi.dy.masa.malilib.util.BlockUtils;
 import fi.dy.masa.malilib.util.StringUtils;
-import net.minecraft.block.BlockState;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 
 public class ToolHud extends InfoHud
 {
@@ -253,6 +252,15 @@ public class ToolHud extends InfoHud
 
                 lines.add(StringUtils.translate("litematica.hud.area_selection.origin", green + str + rst));
 
+                BlockState state = mode.getPrimaryBlock();
+                ItemStack stack = this.mc.player.getMainHandStack();
+
+                if (state != null && mode == ToolMode.REBUILD &&
+                    (stack.isEmpty() || EntityUtils.areStacksEqualIgnoreDurability(stack, DataManager.getToolItem())))
+                {
+                    lines.add(StringUtils.translate("litematica.tool_hud.block_1", this.getBlockString(state)));
+                }
+
                 SubRegionPlacement placement = schematicPlacement.getSelectedSubRegionPlacement();
 
                 if (placement != null)
@@ -312,13 +320,12 @@ public class ToolHud extends InfoHud
 
     protected String getBlockString(BlockState state)
     {
-        ItemStack stack = MaterialCache.getInstance().getItemForState(state);
         String strBlock;
 
         String green = GuiBase.TXT_GREEN;
         String rst = GuiBase.TXT_RST;
 
-        strBlock = green + stack.getName().getString() + rst;
+        strBlock = green + state.getBlock().getName().asString() + rst;
         Direction facing = BlockUtils.getFirstPropertyFacingValue(state);
 
         if (facing != null)

src/main/java/fi/dy/masa/litematica/util/WorldUtils.java
@@ -57,6 +57,7 @@ import fi.dy.masa.litematica.selection.Box;
 import fi.dy.masa.litematica.tool.ToolMode;
 import fi.dy.masa.litematica.util.PositionUtils.Corner;
 import fi.dy.masa.litematica.util.RayTraceUtils.RayTraceWrapper;
+import fi.dy.masa.litematica.util.RayTraceUtils.RayTraceWrapper.HitType;
 import fi.dy.masa.litematica.world.SchematicWorldHandler;
 import fi.dy.masa.litematica.world.WorldSchematic;
 import fi.dy.masa.malilib.gui.GuiBase;
@@ -337,13 +338,26 @@ public class WorldUtils
 
     public static void setToolModeBlockState(ToolMode mode, boolean primary, MinecraftClient mc)
     {
-        HitResult trace = RayTraceUtils.getRayTraceFromEntity(mc.world, mc.player, true, 6);
         BlockState state = Blocks.AIR.getDefaultState();
+        RayTraceWrapper wrapper = RayTraceUtils.getGenericTrace(mc.world, mc.player, 6, true);
 
-        if (trace != null &&
-            trace.getType() == HitResult.Type.BLOCK)
+        if (wrapper != null)
         {
-            state = mc.world.getBlockState(((BlockHitResult) trace).getBlockPos());
+            HitResult trace = wrapper.getBlockHitResult();
+
+            if (trace != null && trace.getType() == HitResult.Type.BLOCK)
+            {
+                BlockPos pos = ((BlockHitResult) trace).getBlockPos();
+
+                if (wrapper.getHitType() == HitType.SCHEMATIC_BLOCK)
+                {
+                    state = SchematicWorldHandler.getSchematicWorld().getBlockState(pos);
+                }
+                else if (wrapper.getHitType() == HitType.VANILLA_BLOCK)
+                {
+                    state = mc.world.getBlockState(pos);
+                }
+            }
         }
 
         if (primary)
@@ -379,7 +393,7 @@ public class WorldUtils
         {
             World world = SchematicWorldHandler.getSchematicWorld();
             BlockState state = world.getBlockState(pos);
-            ItemStack stack = MaterialCache.getInstance().getItemForState(state, world, pos);
+            ItemStack stack = MaterialCache.getInstance().getRequiredBuildItemForState(state, world, pos);
 
             if (stack.isEmpty() == false)
             {
@@ -464,7 +478,7 @@ public class WorldUtils
             BlockPos pos = trace.getBlockPos();
             World world = SchematicWorldHandler.getSchematicWorld();
             BlockState stateSchematic = world.getBlockState(pos);
-            ItemStack stack = MaterialCache.getInstance().getItemForState(stateSchematic);
+            ItemStack stack = MaterialCache.getInstance().getRequiredBuildItemForState(stateSchematic);
 
             // Already placed to that position, possible server sync delay
             if (easyPlaceIsPositionCached(pos))
@@ -767,7 +781,7 @@ public class WorldUtils
             }
 
             BlockState stateSchematic = worldSchematic.getBlockState(pos);
-            stack = MaterialCache.getInstance().getItemForState(stateSchematic);
+            stack = MaterialCache.getInstance().getRequiredBuildItemForState(stateSchematic);
 
             // The player is holding the wrong item for the targeted position
             if (stack.isEmpty() == false && EntityUtils.getUsedHandForItem(mc.player, stack) == null)